feb6ee6e1456b601743bc1a64931af4eea2c891c
QMIQIUQ <dogaang@gmail.com>
2020-12-31 03:44:58 +0800

    test

--- finalProject/resources/views/layouts/app2.blade.php
+++ finalProject/resources/views/layouts/app2.blade.php
@@ -5,16 +5,11 @@
   <title>MiniBarPhone&Services</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
- 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-  </script>
-
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-  </script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
   <style>
     .context-dark,
@@ -127,18 +122,18 @@
       @endif
 
 
-{{-- login --}}
+      {{-- login --}}
       @else
 
 
       <li class="nav-item dropdown">
-        
+
 
         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
           aria-haspopup="true" aria-expanded="false" v-pre>
           @if (Auth::user()->admin == 1)
-      Admin 
-      @endif
+          Admin
+          @endif
           {{ Auth::user()->name }}
         </a>
 
@@ -151,7 +146,7 @@
           <a class="dropdown-item" href="{{url('/showmyCart')}}">
             MyCart
           </a>
-          
+
           <a class="dropdown-item" href="{{url('/insertPhone')}}">
             Add Product to Sell!!
           </a>
@@ -162,7 +157,7 @@
           <a class="dropdown-item" href="{{url('/insertCategory')}}">
             Add Category
           </a>
-          
+
           @endif
 
           <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
@@ -174,7 +169,7 @@
 
 
     </ul>
-    
+
   </nav>
 
   <main class="py-4">
