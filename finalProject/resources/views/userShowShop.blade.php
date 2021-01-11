5c5ac6455340eb95ffa87e8b2ebcbe1054ff6902
LeowHongKai <EP180043C@sc.edu.my>
2021-01-11 12:48:31 +0800

    search for Shop

--- finalProject/resources/views/userShowShop.blade.php
+++ finalProject/resources/views/userShowShop.blade.php
@@ -10,51 +10,47 @@
     	
        
    
-        <div id="content">
+    <div id="content">
        
-        	<h2>Repair Shop</h2>
-        
-            <form action="" method="post">
-            @csrf
+    <h2>Repair Shop</h2>
+             <div>
+                <form action="{{ route('search.shop') }}" method="Get">
+                 @csrf
+                 <div class="input-group">
+                    <input type="text" class="form-control" name="searchShop" id="searchShop" placeholder="Search">
+                    <button class="btn btn-success" type="submit">Search</button>
+                 </div>
+                </form>
+              </div>
+    <form action="" method="post">
+        @csrf
           
-<div class="row">
-     @foreach($shops as $shops)
-  <div class="col-sm-6">
-     <div class="card mb-3 text-center " style="width: 16rem;">
-      
-        <div class="card-body">
-          <h3 class="card-title"><strong>{{$shops->name}}</strong></h3>
+        <div class="row">
+             @foreach($shops as $shops)
+            <div class="col-sm-6">
+            <div class="card mb-3 text-center " style="width: 16rem;">
+                <div class="card-body">
+                    <h3 class="card-title"><strong>{{$shops->name}}</strong></h3>
     
-           <h3 class="card-text " >Rating:{{$shops->ratingPoints}}</h3>
-           <p class="card-text  "style="padding: 0px 0px 0 20px" ></p>
+                    <h3 class="card-text " >Rating:{{$shops->ratingPoints}}</h3>
+                    <p class="card-text  "style="padding: 0px 0px 0 20px" ></p>
          
-          <p class="card-text" ><small class="text-muted">Contact:{{$shops->phoneNumber}}</small></p>
+                    <p class="card-text" ><small class="text-muted">Contact:{{$shops->phoneNumber}}</small></p>
+                </div>
+                <div class="card-footer bg-transparent ">
+                    <small class="text-muted">
+                     <strong>Location:</strong>{{$shops->address}},{{$shops->ZIPcode}},{{$shops->city}},{{$shops->state}},{{$shops->country}}.
+                    </small>
+                </div>
+            </div>
+            </div>
+            @endforeach
         </div>
-        <div class="card-footer bg-transparent ">
-           <small class="text-muted">
-                <strong>Location:</strong>{{$shops->address}},{{$shops->ZIPcode}},{{$shops->city}},{{$shops->state}},{{$shops->country}}.
-           </small>
-        </div>
-
-     </div>
-  </div>
-  @endforeach
-</div>
        
        
-        </form>   
+    </form>   
      
-    
-								
-							
-						
-    
-                
-                
-       
-        </div> <!-- END of content -->
-     
-        <div class="cleaner"></div>
+    </div> <!-- END of content -->
     </div> <!-- END of main -->
     
 </body>    
